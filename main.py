6379c92917988d113a60b0214de1850f6a1f38d1
Mjames35 <marcusj3445@gmail.com>
2022-10-01 16:32:56 +0100

    final neatening

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
 
-
 # INSTALL --- pip install numpy    and pip install plotly
-
 
 import suvatEngine as engine # woop woop my own code works
 import numpy as np
 import plotly.graph_objects as go
 
 
-
-
-fig = go.Figure()
-
-
 def plotter(v,theta,h,name):
     #suvat-engine makes the dataset 
     plot = engine.suvat(float(v),float(theta),float(h))
-    #making the data passable to plotly
+    
+    #grab the data
     x = plot.projectile_x()
     y = plot.projectile_y()
-    y=y.flatten()   
-    x=x.flatten()
-    xl = x.tolist()
-    yl = y.tolist()
+
     #plot the figure
-    fig.add_trace(go.Scatter(x=xl, y=yl,mode="lines", name=name))
+    fig.add_trace(go.Scatter(x=x, y=y,mode="lines", name=name))
     del plot
+
+
+
+fig = go.Figure()
 
 
 
@@ -36,7 +31,6 @@ full input (1) or v+h at all angles (2)?:
 enter b to break, and plot your graph.
 
 """)
-
 
 uin=""
 mode = int(input("1 / 2:  "))
@@ -49,29 +43,23 @@ if mode == 1:
         try:
             plotter(values[0],values[1],values[2],uin)
         except:
-            None
+            print("invalid input, b to break.")
 
 
 
-elif mode == 2:
+else:
     uin=input("v,h:  ")
     values=uin.split(",")
     for i in range(1,90):
         plotter(values[0],i,values[1],i)
-else:
-    None
 
 
 
 #labeling
-fig.update_layout(
-    title="Trajectories (velocity, theta from the verticle, height)", xaxis_title="Horizontal travel (M)", yaxis_title="Vericle travel (M)")
+fig.update_layout(title="Trajectories (velocity, theta from the verticle, height)", xaxis_title="Horizontal travel (M)", yaxis_title="Vericle travel (M)")
 
 #make 1x=1y, as the graph should be life-like.
-fig.update_yaxes(
-    scaleanchor = "x",
-    scaleratio = 1,)
-
+fig.update_yaxes(scaleanchor = "x", scaleratio = 1,)
 
 #plot figure
 fig.show()
--- suvatEngine.py
+++ suvatEngine.py
@@ -3,26 +3,38 @@ import math
 
 class suvat:
     def __init__(self,v,theta,h):
-        self.g = 9.81
+        self.g = 9.81 
         self.v = v
         self.h = h
-        self.theta = theta
-        self.rads = theta*(np.pi/180)
+        self.theta = theta # from the verticle
+        self.rads = theta*(np.pi/180) # putting theta in rads for numpy
+
+        #verticle and horizontal componants of v at t=0.
         self.v0y = v * np.cos(self.rads)
         self.v0x = v * np.sin(self.rads) 
+
+        #flight time
         if h ==0:
             self.maxt = (2*self.v0y)/self.g
         else:
-            self.maxt = (math.sqrt(2*self.g*self.h+(self.v0y**2))+self.v0y)/self.g
+            self.maxt = (math.sqrt(2*self.g*self.h+(self.v0y**2))+self.v0y)/self.g 
 
-        self.timearray = np.linspace(0,self.maxt,2000)[:,None] # array from 0 to t, 100 increments.
+        # array from 0 to t, 2000 increments, used to calculate
+        self.timearray = np.linspace(0,self.maxt,2000)[:,None]
+
+
 
     def projectile_x(self):
-        return (self.timearray*self.v0x) # horizontal displacement x=HorizS
+        x=(self.timearray*self.v0x) # horizontal displacement for the x axis
+        x=x.flatten()  # formatting for the graphing engine
+        return x.tolist()
+
 
     def projectile_y(self):
-        return (self.timearray*self.v0y)-((self.g/2)*(self.timearray**2))+self.h # suvat equation moment, verticle displacement, y=vertS
-
+        y=(self.timearray*self.v0y)-((self.g/2)*(self.timearray**2))+self.h # verticle displacement for the y axis
+        y=y.flatten()   # formatting for the graphing engine
+        return y.tolist()
+    
 
 
 
